3e3dce582d99bf75fa7a53919440f5b1f52754ca
hewenjie95 <594060684@qq.com>
2017-07-19 09:50:46 +0800

    部门查询ajax

--- ESMS/js/query_dept.js
+++ ESMS/js/query_dept.js
@@ -1,34 +1,15 @@
-$(function(){
-	urlmessage();
+$('.choseQuerydept').on('click',function(){
+	querydept();
 });
 
 
-function urlmessage(){
+function querydept(){
 	$('.btn_query_dept').click(function(){
 		
-		var deptno = $('.get_Deptno').val();
-		var dname = $('.get_Dname').val();
-		var loginUrl = 'esms/login';
-		if($('.login_name').val()==""&&$('.login_psw').val()==""){
-			layer.msg('账号密码不能为空！',function(){
-				return false;
-			});
-			return false;
-		};
-		if($('.login_name').val()==""&&$('.login_psw').val()!=""){
-			layer.msg('账号不能为空！',function(){
-				return false;
-			});
-			return false;
-		};
-		if($('.login_name').val()!=""&&$('.login_psw').val()==""){
-			layer.msg('密码不能为空！',function(){
-				return false;
-			});
-			return false;
-		};
-		if($('.login_name').val().length>10){
-			layer.msg('账号长度不能大于20',function(){
+		var deptcondition = $('.get_deptcondition').val();
+		var loginUrl = 'dept/query';
+		if($('.get_deptcondition').val()==""){
+			layer.msg('查询条件不能为为空！',function(){
 				return false;
 			});
 			return false;
@@ -36,13 +17,22 @@ function urlmessage(){
 		/*console.log(user_name);
 		console.log(user_password);*/
 		var param = {
-				usr: deptno,
-				pwd: dname
+				query: eptcondition
 		};
 		// param = JSON.stringify(param);
 		publicDom.post(loginUrl,param,function(response){
 				if (response.code == 1001) {
-					$('dept_manage_div').append("<div class=""></div>");
+					$('.dept_manage_div').append("<div class="panel panel-default hide_adddiv adddiv_querydept">"+
+						"<div class="panel-heading">部门信息</div>"+
+						"<table class="table addtr_querydept">"+
+						"<tbody><tr><th>编号</th><th>名称</th><th>所在地</th></tr></tbody>"+	
+						"</table></div>");
+					var dept = response.data;
+					for( var i=0;i < dept.length;i++ ){
+						$('.addtr_querydept').append(
+							"<tr><td>dept[i].deptno</td><td>dept[i].dname</td><td>dept[i].loc</td></tr>");
+					}
+					
 				}
 				else {
 					layer.msg(response.message);
